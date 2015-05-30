1b08207745a51da5013573f14473f8cbddf0f8f4
Carter De Angelis <me@carterdea.com>
2015-05-30 01:22:43 -0700

    Lessons 4-9, models, migrations, helpers, & routes

--- crowdfund/app/helpers/projects_helper.rb
+++ crowdfund/app/helpers/projects_helper.rb
@@ -1,2 +1,9 @@
 module ProjectsHelper
+  def time_remaining(project)
+    if project.past? || project.blank?
+      content_tag(:strong, "All done!")
+    else
+      distance_of_time_in_words(Time.now, project) + " remaining"
+    end
+  end
 end
